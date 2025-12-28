e18014acb2d99655cecea6f23e80e2c30ec4fb05
Vangala Uday Kiran Reddy <vangalaudaykiranreddy@gmail.com>
2025-12-28 13:18:00 +0530

    Blog app and model

---
+++ blogs/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category,Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('title',)}
+    list_display=('title','author','category','is_featured','status')
+    search_fields=('id','title','category__category_name','status')
+    list_editable=('is_featured',)
+    
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
